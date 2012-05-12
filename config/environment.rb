25822d4be65dd2b118eb7c869350f3b284499617
Jerome Touffe-Blin <jtouffeblin@expedia.com>
2012-05-13 01:35:50 +1000

    Default to oj json, logger for ActiveResource

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Agora::Application.initialize!
+
+ActiveResource::Base.logger = Rails.logger
+
+ActiveSupport::JSON.backend = 'oj'
